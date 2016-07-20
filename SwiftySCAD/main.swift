f2fd180900fd52e98373dbb54296bb0e72ca32e1
Jeffrey Camealy <jeffrey@theworklog.com>
2016-07-19 18:47:48 -0700

    Initial Commit

---
+++ SwiftySCAD/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftySCAD
+//
+//  Created by Jeffrey Camealy on 7/19/16.
+//  Copyright © 2016 Branch Computing. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
